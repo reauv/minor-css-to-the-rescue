635fa1431b536b6135ce47828dc0ab46a556806a
reauv <studio@rovansteen.nl>
2016-02-09 13:07:05 +0100

    Duplicate exercise 1 for exercise 2

---
+++ exercise_2/gulpfile.js
@@ -0,0 +1,35 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch');
+var stylus = require('gulp-stylus');
+var plumber = require('gulp-plumber');
+var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
+
+gulp.task('css', function () {
+  return gulp.src('./src/stylus/main.styl')
+    .pipe(sourcemaps.init())
+    .pipe(stylus())
+    .pipe(autoprefixer({
+        browsers: ['last 2 versions'],
+        cascade: false,
+        grid: false,
+    }))
+    .pipe(sourcemaps.write('.'))
+    .pipe(gulp.dest('./build/css'))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('browser-sync', function() {
+    browserSync.init({
+        server: {
+            baseDir: "./build"
+        }
+    });
+
+    gulp.watch('./src/stylus/**/*.styl', ['css']);
+    gulp.watch("./build/*.html").on('change', browserSync.reload);
+});
+
+
+gulp.task('default', ['css', 'browser-sync']);
